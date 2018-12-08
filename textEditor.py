56aa83e6a117aa993f58d1ab31b1066896ac8042
Anuadh <40334668+Anuadh@users.noreply.github.com>
2018-12-08 15:12:53 +0530

    Code

---
+++ textEditor.py
@@ -0,0 +1,51 @@
+from Tkinter import *
+from tkFileDialog import *
+
+filename = None
+
+def newFile():
+    global filename
+    filename = "Untitled"
+    text.delete(0.0, END)
+
+def saveFile():
+    global filename
+    t = text.get(0.0, END)
+    f = open(filename, 'w')
+    f.write(t)
+    f.close()
+
+def saveAs():
+    f = asksaveasfile(mode='w', defaultextension='.txt')
+    t = text.get(0.0,END)
+    try:
+        f.write(t.rstrip())
+    except:
+        showerror(title="Oops!", message="Unable to save file...")
+
+def openFile():
+    f = askopenfile(mode='r')
+    t = f.read()
+    text.delete(0.0, END)
+    text.insert(0.0, t)
+
+root = Tk()
+root.title("Anubhav Text Editor")
+root.minsize(width=400, height=400)
+root.maxsize(width=400, height=400)
+
+text =Text(root, width=400, height=400)
+text.pack()
+
+menubar = Menu(root)
+filemenu = Menu(menubar)
+filemenu.add_command(label="New", command=newFile)
+filemenu.add_command(label="Open", command=openFile)
+filemenu.add_command(label="Save", command=saveFile)
+filemenu.add_command(label="Save As...", command=saveAs)
+filemenu.add_separator()
+filemenu.add_command(label="Quit", command=root.quit)
+menubar.add_cascade(label="File", menu=filemenu)
+
+root.config(menu=menubar)
+root.mainloop()
